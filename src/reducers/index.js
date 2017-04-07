c531dda023495723ff1e98c07789925b9b6ab9d7
Neous1 <Neous1@users.noreply.github.com>
2017-04-07 10:06:47 -0400

    2nd commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BookReducer from "./reducer_books";
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+//  state: (state = {}) => state
+    books: BooksReducer    
 });
 
 export default rootReducer;
